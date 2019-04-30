e46eeb4a1979ba7ff64254897c4cddf2be13f312
bfpulliam <bfpulliam@gmail.com>
2019-04-29 23:43:31 -0500

    upload to heroku

--- README.md
+++ README.md
@@ -4,12 +4,12 @@
 A handlebars app where MySQL and Node.js are used to consume burgers that are stored in MySQL. The user enters a burger, which gets added to the database and if the burger is consumed, it's considered devoured. The user can continue to add new burgers.
 
 ## Technologies Used:
-    *Javascript
-    *Node JS
-    *MySQL
-    *Mysql-npm
-    *Express JS
-    *Express-handlebars JS
+    * Javascript
+    * Node JS
+    * MySQL
+    * Mysql-npm
+    * Express JS
+    * Express-handlebars JS
 
 ## Contact
 Developer/Full-stack Web Software Developer (in training)
--- models/burger.js
+++ models/burger.js
@@ -8,16 +8,24 @@ var burger = {
         });
     },
     // The variables cols and vals are arrays.
-    insertOne: function (cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function (res) {
+    insertOne: function (name, cb) {
+        orm.insertOne("burgers", [
+
+            "burger_name", "devoured"
+
+        ], [
+
+            name, false
+
+        ], cb(res));
+
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
         });
     },
-    updateOne: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    }
+    
 };
 
 // Export the database functions for the controller (burger_controller.js).
