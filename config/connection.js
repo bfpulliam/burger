bcc8fcff000cc960619dd0e3c71ccf47ec447a7f
bfpulliam <bfpulliam@gmail.com>
2019-04-30 00:42:50 -0500

    jaws added

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+
 require('dotenv').config();
+
 var password = process.env.CODE;
-var connection = mysql.createConnection({
+
+var connection; 
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}  else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: password,
     database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function (err) {
     if (err) {
